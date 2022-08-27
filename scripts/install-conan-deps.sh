d5a1cca7146ee86b50957dfdcd80ef77b6366ccd
Carlos Chulo <cchulo.dev@gmail.com>
2022-08-27 05:47:42 -0700

    had to add profile for arch-based distros, will need to automate this somehow, okay to proceed for now

--- scripts/install-conan-deps.sh
+++ scripts/install-conan-deps.sh
@@ -3,4 +3,4 @@
 echo "$CONAN_SYSREQUIRES_MODE"
 
 echo 'Restoring Conan packages'
-conan install . -if bin/conan -of bin --profile default.jinja --profile compiler-linux-clang.jinja --build=missing -s build_type=Release
+conan install . -if bin/conan -of bin --profile default.jinja --profile compiler-linux-clang.jinja --profile host-arch.jinja --build=missing -s build_type=Release
