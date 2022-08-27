125aac9e7bda0728d6d1deb0d494dba6875edaba
Carlos Chulo <cchulo.dev@gmail.com>
2022-08-27 03:37:50 -0700

    added script for restoring conan deps, but have an issue where Conan does not know how to install dependencies for EndeavourOS, may need to edit their recipe

---
+++ scripts/install-conan-deps.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "$CONAN_SYSREQUIRES_MODE"
+
+echo 'Restoring Conan packages'
+conan install . -if bin/conan -of bin --profile default.jinja --profile compiler-linux-clang.jinja --build=missing -s build_type=Release
