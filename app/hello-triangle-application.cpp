ceffcc04459cf01ec15e5f966eb37d79db606c1b
Carlos Chulo <cchulo.dev@gmail.com>
2022-09-19 22:43:29 -0700

    started with creating Vulkan instance, saving progress

--- app/hello-triangle-application.cpp
+++ app/hello-triangle-application.cpp
@@ -1,24 +1,63 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "OCUnusedMacroInspection"
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+const uint32_t kWidth = 800;
+const uint32_t kHeight = 600;
 
 class HelloTriangleApplication {
  public:
   void run() {
+    initWindow();
     initVulkan();
     mainLoop();
     cleanup();
   }
 
  private:
-  void initVulkan() {
 
+  GLFWwindow *window_;
+  VkInstance instance_;
+  void initWindow() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window_ = glfwCreateWindow(kWidth, kHeight, "Vulkan", nullptr, nullptr);
+  }
+
+  void createInstance() {
+    VkApplicationInfo app_info{};
+    app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    app_info.pApplicationName = "Hello Triangle";
+    app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    app_info.pEngineName = "No Engine";
+    app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    app_info.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo create_info{};
+    create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  }
+
+  void initVulkan() {
+    createInstance();
   }
 
   void mainLoop() {
-
+    while (!glfwWindowShouldClose(window_)) {
+      glfwPollEvents();
+    }
   }
 
   void cleanup() {
-
+    glfwDestroyWindow(window_);
+    glfwTerminate();
   }
 };
 
@@ -33,3 +72,5 @@ int main() {
   }
   return EXIT_SUCCESS;
 }
+
+#pragma clang diagnostic pop
