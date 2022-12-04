cf0e213854abb899182bf7107c03bcd3801655ba
Carlos Chulo <cchulo.dev@gmail.com>
2022-12-04 03:46:51 -0800

    completed Image Views

--- app/hello-triangle-application.cpp
+++ app/hello-triangle-application.cpp
@@ -84,6 +84,7 @@ class HelloTriangleApplication { // NOLINT(cppcoreguidelines-pro-type-member-ini
   std::vector<VkImage> _swapChainImages;
   VkFormat _swapChainImageFormat;
   VkExtent2D _swapChainExtent;
+  std::vector<VkImageView> _swapChainImageViews;
 
   void InitWindow() {
     glfwInit();
@@ -216,6 +217,7 @@ class HelloTriangleApplication { // NOLINT(cppcoreguidelines-pro-type-member-ini
     PickPhysicalDevice();
     CreateLogicalDevice();
     CreateSwapChain();
+    CreateImageViews();
   }
 
   void CreateSurface() {
@@ -502,6 +504,33 @@ class HelloTriangleApplication { // NOLINT(cppcoreguidelines-pro-type-member-ini
     }
   }
 
+  void CreateImageViews() {
+    _swapChainImageViews.resize(_swapChainImages.size());
+
+    for (size_t i = 0; i < _swapChainImages.size(); i++) {
+      VkImageViewCreateInfo createInfo{};
+      createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+      createInfo.image = _swapChainImages[i];
+      createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+      createInfo.format = _swapChainImageFormat;
+
+      createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+      createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+      createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+      createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+      createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+      createInfo.subresourceRange.baseMipLevel = 0;
+      createInfo.subresourceRange.levelCount = 1;
+      createInfo.subresourceRange.baseArrayLayer = 0;
+      createInfo.subresourceRange.layerCount = 1;
+
+      if (vkCreateImageView(_device, &createInfo, nullptr, &_swapChainImageViews[i]) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create image views!");
+      }
+    }
+  }
+
   static void PopulateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT &createInfo) {
     createInfo = {};
     createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
@@ -538,6 +567,10 @@ class HelloTriangleApplication { // NOLINT(cppcoreguidelines-pro-type-member-ini
   }
 
   void Cleanup() {
+    for (auto imageView : _swapChainImageViews) {
+      vkDestroyImageView(_device, imageView, nullptr);
+    }
+
     vkDestroySwapchainKHR(_device, _swapChain, nullptr);
     vkDestroyDevice(_device, nullptr);
 
